85dc0008be6eb9682bb814135dcce9f78c5eadf1
santoshvnit13 <108055937+santoshvnit13@users.noreply.github.com>
2023-04-02 14:27:04 +0530

    Update index.js

--- src/components/LatestMatch/index.js
+++ src/components/LatestMatch/index.js
@@ -1,1 +1,39 @@
 // Write your code here
+const LatestMatch = props => {
+  const {latestMatchDetails} = props
+  const {
+    umpires,
+    result,
+    manOfTheMatch,
+    id,
+    date,
+    venue,
+    competingTeam,
+    competingTeamLogo,
+    firstInnings,
+    secondInnings,
+    matchStatus,
+  } = latestMatchDetails
+
+  return (
+    <li>
+      <p>{competingTeam}</p>
+      <p>{date}</p>
+      <p>{venue}</p>
+      <p>{result}</p>
+      <img src={competingTeamLogo} alt={`latest match ${competingTeam}`} />
+      <hr />
+
+      <p>First Innings</p>
+      <p>{firstInnings}</p>
+      <p>Second Innings</p>
+      <p>{secondInnings}</p>
+      <p>Man Of The Match</p>
+      <p>{manOfTheMatch}</p>
+      <p>Umpires</p>
+      <p>{umpires}</p>
+    </li>
+  )
+}
+
+export default LatestMatch
