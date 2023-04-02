a5456c87767319b57f0b45af63dcf0b6007620eb
santoshvnit13 <108055937+santoshvnit13@users.noreply.github.com>
2023-04-02 14:25:02 +0530

    Update index.js

--- src/components/TeamMatches/index.js
+++ src/components/TeamMatches/index.js
@@ -1,1 +1,98 @@
 // Write your code here
+// Write your code here
+import {Component} from 'react'
+
+import Loader from 'react-loader-spinner'
+
+import LatestMatch from '../LatestMatch'
+
+import MatchCard from '../MatchCard'
+
+class TeamMatches extends Component {
+  state = {
+    isLoader1: true,
+    teamBannerUrl: '',
+    latestMatchDetails: {},
+    recentMatches: [],
+  }
+
+  componentDidMount() {
+    this.matchDetails()
+  }
+
+  matchDetails = async () => {
+    const {match} = this.props
+    const {params} = match
+    const {id} = params
+    const response = await fetch(`https://apis.ccbp.in/ipl/${id}`)
+    const data = await response.json()
+    const updatedTeamBannerUrl = data.team_banner_url
+    const updatedLatestMatchDetails = {
+      umpires: data.latest_match_details.umpires,
+      result: data.latest_match_details.result,
+      manOfTheMatch: data.latest_match_details.man_of_the_match,
+      id: data.latest_match_details.id,
+      date: data.latest_match_details.date,
+      venue: data.latest_match_details.venue,
+      competingTeam: data.latest_match_details.competing_team,
+      competingTeamLogo: data.latest_match_details.competing_team_logo,
+      firstInnings: data.latest_match_details.first_innings,
+      secondInnings: data.latest_match_details.second_innings,
+      matchStatus: data.latest_match_details.match_status,
+    }
+
+    const updatedRecentMatches = data.recent_matches.map(eachItem => ({
+      umpires: eachItem.umpires,
+      result: eachItem.result,
+      manOfTheMatch: eachItem.man_of_the_match,
+      id: eachItem.id,
+      date: eachItem.date,
+      venue: eachItem.venue,
+      competingTeam: eachItem.competing_team,
+      competingTeamLogo: eachItem.competing_team_logo,
+
+      firstInnings: eachItem.first_innings,
+      secondInnings: eachItem.second_innings,
+      matchStatus: eachItem.match_status,
+    }))
+
+    this.setState({
+      isLoader1: false,
+      teamBannerUrl: updatedTeamBannerUrl,
+      latestMatchDetails: updatedLatestMatchDetails,
+      recentMatches: updatedRecentMatches,
+    })
+  }
+
+  render() {
+    const {
+      isLoader1,
+      teamBannerUrl,
+      latestMatchDetails,
+      recentMatches,
+    } = this.state
+    return (
+      <>
+        {isLoader1 ? (
+          <div testid="loader">
+            {' '}
+            <Loader type="Oval" color="#ffffff" height={50} width={50} />{' '}
+          </div>
+        ) : (
+          <>
+            <img src={teamBannerUrl} alt="team banner" />
+            <h1>Latest Matches</h1>
+            <LatestMatch latestMatchDetails={latestMatchDetails} />
+            <ul>
+              {recentMatches.map(recentMatch => (
+                <MatchCard key={recentMatch.id} recentMatch={recentMatch} />
+              ))}
+            </ul>
+          </>
+        )}
+      </>
+    )
+  }
+}
+
+export default TeamMatches
