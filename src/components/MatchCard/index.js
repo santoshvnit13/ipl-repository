c5ff90668935714e49a7ce759c9924ef9f49b3e2
santoshvnit13 <108055937+santoshvnit13@users.noreply.github.com>
2023-04-02 14:26:31 +0530

    Update index.js

--- src/components/MatchCard/index.js
+++ src/components/MatchCard/index.js
@@ -1,1 +1,17 @@
 // Write your code here
+const MatchCard = props => {
+  const {recentMatch} = props
+  const {matchStatus, result, competingTeam, competingTeamLogo} = recentMatch
+
+  return (
+    <li>
+      <img src={competingTeamLogo} alt={`competing team ${competingTeam}`} />
+      <p>{competingTeam}</p>
+      <p>{result}</p>
+      <p>{matchStatus}</p>
+    </li>
+  )
+}
+
+export default MatchCard
+
